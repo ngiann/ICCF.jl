e19e7dfd74656e65295148895a45425ddc120631
ngiann <ngiann@gmail.com>
2022-10-26 12:57:25 +0200

    remove temporary file

--- src/bootstrapanalysis.jl
+++ src/bootstrapanalysis.jl
@@ -1,4 +1,26 @@
-function bootstrapanalysis(; repeats = 1000, t1 = t1, t2 = t2, y1 = y1, y2 = y2, σ1 = σ1, σ2 = σ2, minτ = 0.0, maxτ = maxτ, dτ = 0.1, seed₀ = 1, threshold = 0.8, showplot = false)
+"""
+    centroids = bootstrapanalysis(; t1 = t1, t2 = t2, y1 = y1, y2 = y2, σ1 = σ1, σ2 = σ2, minτ = 0.0, maxτ = maxτ, dτ = 0.1, repeats = 1000, threshold = 0.8, seed = 1, showplot = false)
+
+Given a pair of lightcurves returns the distribution of the centroid as obtained via bootstrapping.
+
+# Arguments
+# Arguments
+- `t1` are the observed times of the first lightcurve.
+- `t2` are the observed times of the second lightcurve.
+- `y1` are the observed fluxes of the first lightcurve.
+- `y2` are the observed fluxes of the second lightcurve.
+- `minτ` is the minimum considered delay, default value is 0.
+- `maxτ` is the maximum considered delay.
+- `dτ` is the delay step size, default value is 0.1.
+- `seed` controls the random number generator. Default is `1`.
+- `repeats` is the number of times that ICCF is repeated for. Each repeatition produces a sample for the centroid. Default is `1000`.
+- `threshold` specifies the interval around the peak of the ICCF curve used to calculate the centroid.
+- If `showplot` is set to `true`, a plot of the produced ICCF curves will appear. Default is `false`. 
+
+# Outputs
+- `centroids` holds the sampled centroids.
+"""
+function bootstrapanalysis(; t1 = t1, t2 = t2, y1 = y1, y2 = y2, σ1 = σ1, σ2 = σ2, minτ = 0.0, maxτ = maxτ, dτ = 0.1, repeats = 1000, threshold = 0.8, seed = 1, showplot = false)
 
     if showplot
         figure(); title(@sprintf("%d average curves", repeats))
@@ -8,10 +30,10 @@ function bootstrapanalysis(; repeats = 1000, t1 = t1, t2 = t2, y1 = y1, y2 = y2,
 
     @showprogress for i in 1:repeats
 
-        
-        t1res, y1res = resamplingwithnoise(t = t1, y = y1, σ = σ1, seed = seed₀ + i)
 
-        t2res, y2res = resamplingwithnoise(t = t2, y = y2, σ = σ2, seed = seed₀ + i + repeats)
+        t1res, y1res = resamplingwithnoise(t = t1, y = y1, σ = σ1, seed = seed + i)
+
+        t2res, y2res = resamplingwithnoise(t = t2, y = y2, σ = σ2, seed = seed + i + repeats)
 
 
         delays12, ccf12    = iccf(t1 = t1res, t2 = t2res, y1 = y1res, y2 = y2res, minτ = minτ, maxτ = maxτ, dτ = dτ, showplot = false)
@@ -33,7 +55,6 @@ function bootstrapanalysis(; repeats = 1000, t1 = t1, t2 = t2, y1 = y1, y2 = y2,
 
         centroidarray[i] = centroid(τ = commondelays, avgcurve = avgcurve, threshold = threshold)
         
-
     end
     
     return centroidarray
--- src/resamplingwithnoise.jl
+++ src/resamplingwithnoise.jl
@@ -1,3 +1,19 @@
+"""
+    tres, yres = resamplingwithnoise(; t = t, y = y, σ = σ, seed = 1, scalebysqrt = true)
+
+Produces a resampled version of given lightcurve t, y.
+
+# Arguments
+- `t` are the observed times of lightcurve.
+- `y` are the observed fluxes of lightcurve.
+- `σ` are the measurement errors of the lightcurve.
+- `seed` controls the random number generator that samples the lightcurve and the added noise. Default value is `1`.
+- `scalebysqrt` scales `σ` by the square root of the number of points in resampled lightcurve. Default value is `true`.
+
+# Outputs
+- `tres` holds the resampled times, in ascending order.
+- `yres` holds the resampled lightcurve.
+"""
 function resamplingwithnoise(; t = t, y = y, σ = σ, seed = 1, scalebysqrt = true)
 
     rg = MersenneTwister(seed)
